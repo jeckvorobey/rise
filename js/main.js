79964e3de90f84f7fc36f2cd3aa94f9c2d217e01
Jecjvorobey <jeckvorobey69@gmail.com>
2021-08-04 12:07:42 +0300

    подключил barba, gzap, пофиксил стили

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
+
+
 window.onload = function () {
+
   let img = document.getElementById('image');
   const modalContent = document.querySelector('.modal-content')
   let modalImage = document.createElement('img')
   image.setAttribute('src', img.getAttribute('src'))
   image.setAttribute('alt', img.getAttribute('alt'))
   modalContent.appendChild(modalImage);
+
+
+  barba.init({
+    transitions: [{
+      name: 'opacity-transition',
+      leave(data) {
+        return gsap.to(data.current.container, {
+          opacity: 0
+        });
+      },
+      enter(data) {
+        return gsap.from(data.next.container, {
+          opacity: 0
+        });
+      }
+    }]
+  });
 }
