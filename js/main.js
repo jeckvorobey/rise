57dfa8e69536a3fe521a46e31da02d33b7c34b35
Jecjvorobey <jeckvorobey69@gmail.com>
2021-08-04 10:24:00 +0300

    сделал модалку

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  let img = document.getElementById('image');
+  const modalContent = document.querySelector('.modal-content')
+  let modalImage = document.createElement('img')
+  image.setAttribute('src', img.getAttribute('src'))
+  image.setAttribute('alt', img.getAttribute('alt'))
+  modalContent.appendChild(modalImage);
+}
